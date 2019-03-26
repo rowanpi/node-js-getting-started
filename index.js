c3ace2c084014d5ad071ae3f4676632052917718
rowan84 <rowan.pillay@jembi.org>
2019-03-26 18:13:53 +0200

    Remove post to localhost

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function postRecievedResponse(request) {
   data.append('from',request.body.to);
   data.append('text',request.body.text);
   data.append('date',new Date().toISOString());
-  //axios.post('https://app.rapidpro.io/c/ex/8c9b878c-d395-4685-88ac-501532ed877d/receive', data)
-  axios.post('http://localhost:5000/test', data)
+  axios.post('https://app.rapidpro.io/c/ex/8c9b878c-d395-4685-88ac-501532ed877d/receive', data)
+  //axios.post('http://localhost:5000/test', data)
   .then((res) => {
     console.log(`statusCode: ${res.status}`)
     console.log(res);
